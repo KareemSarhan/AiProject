7635ae6893ff45f3ae9b93b5c67da793412d8be6
Kareem <Kareemsarhan@gmail.com>
2021-10-31 14:42:44 +0200

    implement genGrid

--- AiProject/src/Matrix.java
+++ AiProject/src/Matrix.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.Vector;
+
 public class Matrix {
     /**
      * Generates a random grid.
@@ -35,7 +38,87 @@ public class Matrix {
      */
     public static String genGrid()
     {
-        return "";
+        int gridX = getRandom(5,15);
+        int gridY = getRandom(5,15);
+        int numOfHostages = getRandom(3,10);
+        int[] damagePerHostage = new int[numOfHostages];
+        for (int i = 0; i < numOfHostages; i++) {
+            damagePerHostage[i] = getRandom(1,99);
+        }
+        int carryLimit = getRandom(1,4);
+        int numOfPills = getRandom(1,numOfHostages);
+        Vector<int []> emptySlots = new Vector<int []>();
+        for (int i = 0; i < gridX; i++) {
+            for (int j = 0; j < gridY; j++) {
+                emptySlots.add(new int[]{i,j});
+            }
+        }
+        String grid = "";
+        grid += gridX+","+gridY;
+        grid += ";" + carryLimit;
+        int[] neoStartCell = emptySlots.remove(getRandom(0,emptySlots.size()));
+        grid += ";" + neoStartCell[0]+","+neoStartCell[1];
+        int[] telephoneBoothCell = emptySlots.remove(getRandom(0,emptySlots.size()));
+        grid += ";" + telephoneBoothCell[0]+","+telephoneBoothCell[1];
+        int[][] pillCells = new int[numOfPills][2];
+        for (int i = 0; i < numOfPills; i++) {
+            pillCells[i] = emptySlots.remove(getRandom(0,emptySlots.size()));
+        }
+        int[][] hostageCells = new int[numOfHostages][2];
+        for (int i = 0; i < numOfHostages; i++) {
+            hostageCells[i] = emptySlots.remove(getRandom(0,emptySlots.size()));
+        }
+        int numOfAgents = getRandom(1,emptySlots.size()-1);
+        int[][] agentsCells = new int[numOfAgents][2];
+        for (int i = 0; i < numOfAgents; i++) {
+            agentsCells[i] = emptySlots.remove(getRandom(0,emptySlots.size()));
+        }
+        // TODO: 10/31/2021 Check if next line is right
+        int numOfPads = getRandom(1,emptySlots.size()-1)/2;
+        int[][] padsCells = new int[numOfPads][4];
+        for (int i = 0; i < numOfPads; i++) {
+            int[] padStartCell = emptySlots.remove(getRandom(0,emptySlots.size()));
+            int[] padEndCell = emptySlots.remove(getRandom(0,emptySlots.size()));
+            padsCells[i][0] = padStartCell[0];
+            padsCells[i][1] = padStartCell[1];
+            padsCells[i][2] = padEndCell[0];
+            padsCells[i][3] = padEndCell[1];
+        }
+        grid +=";";
+        for (int i = 0; i < numOfAgents; i++) {
+            grid += agentsCells[i][0] + "," +agentsCells[i][1] ;
+            if (i < numOfAgents-1) {grid +=  ",";
+            }
+        }
+        grid +=";";
+        for (int i = 0; i < numOfPills; i++) {
+            grid += pillCells[i][0] + "," +pillCells[i][1] ;
+            if (i < numOfPills-1) {grid +=  ",";
+            }
+        }
+        grid +=";";
+        for (int i = 0; i < numOfPads; i++) {
+            grid += padsCells[i][0] + "," +padsCells[i][1] + "," +padsCells[i][2] + "," +padsCells[i][3] ;
+            if (i < numOfPads-1) {grid +=  ",";
+            }
+        }
+        grid +=";";
+        for (int i = 0; i < numOfHostages; i++) {
+            grid += hostageCells[i][0] + "," +hostageCells[i][1] + "," +damagePerHostage[i] ;
+            if (i < numOfHostages-1) {grid +=  ",";
+            }
+        }
+        /**
+         // All Prints
+        System.out.println("tele" + Arrays.toString(telephoneBoothCell));
+        System.out.println("agents" + Arrays.deepToString(agentsCells));
+        System.out.println("pads" + Arrays.deepToString(padsCells));
+        System.out.println("pills" + Arrays.deepToString(pillCells));
+        System.out.println("neo" + Arrays.toString(neoStartCell));
+        System.out.println("hostages" + Arrays.deepToString(hostageCells));
+        System.out.println("hostagesDmg" + Arrays.toString(damagePerHostage));
+         */
+        return grid;
     }
 
     /**
@@ -71,5 +154,10 @@ public class Matrix {
     public static void Visualize(String grid)
     {
 
+    }
+    // TODO: 10/31/2021 Check getRandom , Check if its used correctly
+    public static int getRandom(int min, int max) {
+        int rand = (int) ((Math.random() * (max - min)) + min);
+        return rand;
     }
 }
--- AiProject/src/main.java
+++ AiProject/src/main.java
@@ -1,5 +1,6 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("Welcome to the Matrix.");
+        System.out.println(Matrix.genGrid());
     }
 }
